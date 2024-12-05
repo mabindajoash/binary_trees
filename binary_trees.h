23ce2923f2a69e64469ddc698002ffc21d473e07
mabindajoash <mabindajoash@gmail.com>
2024-12-05 11:01:28 -0800

    struct

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,17 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
