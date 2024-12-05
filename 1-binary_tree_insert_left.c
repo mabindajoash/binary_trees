92621c33d0ab3ad109716ec59e1d1a58f5427475
mabindajoash <mabindajoash@gmail.com>
2024-12-05 07:44:10 -0800

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,9 +13,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 	new_node->parent = parent;
-	new_node->left = NULL;
-	parent->left = new_node;
 	new_node->right = NULL;
 	new_node->n = value;
+	new_node->left = parent->left;
+	if (parent->left)
+		parent->left->parent = new_node;
+	parent->left = new_node;
 	return (new_node);
 }
