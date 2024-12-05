40a090b1c741fd99098fe60399aab0f1aa13160c
mabindajoash <mabindajoash@gmail.com>
2024-12-05 10:02:03 -0800

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,5 +9,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	func(tree)
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
